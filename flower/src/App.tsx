e76c97b500a81df6f537915ad591ae051e50628e
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-10-31 22:31:53 +0900

    created route

--- flower/src/App.tsx
+++ flower/src/App.tsx
@@ -1,5 +1,29 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import NotFound from "./components/notfound";
+import Layout from "./components/layout";
+import Home from "./screen/home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        errorElement: <NotFound />,
+        children: [
+            {
+                path: "",
+                element: <Home />,
+            },
+        ],
+    },
+]);
+
 function App() {
-    return null;
+    return (
+        <>
+            <RouterProvider router={router} />
+        </>
+    );
 }
 
 export default App;
--- flower/src/index.tsx
+++ flower/src/index.tsx
@@ -1,7 +1,28 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { reset } from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  body {
+    background-color: black;
+    color: whitesmoke;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  * {
+    box-sizing: border-box;
+    text-decoration: none;
+  }
+`;
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+    <>
+        <GlobalStyles />
+        <App />
+    </>
+);
