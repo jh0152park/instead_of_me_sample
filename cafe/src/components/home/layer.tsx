a8cf06c5b41d55f443b0bb23a268bde5d35207f5
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-10-31 14:59:59 +0900

    main page scroll done

--- cafe/src/components/home/layer.tsx
+++ cafe/src/components/home/layer.tsx
@@ -13,14 +13,14 @@ export default function Layer() {
 
     const { scrollY } = useScroll();
 
-    const scrollTH = 30;
+    const scrollTH = 25;
     const [scrollStack, setScrollStack] = useState<number>(0);
     const [prevY, setPrevY] = useState<number>(0);
     const [layer, setLayer] = useState<number>(1);
 
     useMotionValueEvent(scrollY, "change", (latest) => {
         console.log(`y: ${latest}`);
-        if (latest < 0 || latest > 3000) return;
+        if (latest < 0 || latest > 2800) return;
 
         setScrollStack((prev) => prev + 1);
         if (scrollStack > scrollTH) {
