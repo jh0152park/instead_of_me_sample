5df0ae3bfa565d4ad7a367231c8fc3b0c4889867
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-31 21:39:44 +0900

    Added header color animation

---
+++ logistics/src/components/home/innovation.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+
+export default function Innovation() {
+    return (
+        <Box
+            w={"100dvw"}
+            h={"100dvh"}
+            backgroundColor={"white"}
+            color={"black"}
+        ></Box>
+    );
+}
