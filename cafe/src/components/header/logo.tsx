92851b0180130396c646503cf25de692b9a5b7de
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-10-31 16:21:56 +0900

    created header done

--- cafe/src/components/header/logo.tsx
+++ cafe/src/components/header/logo.tsx
@@ -1,9 +1,19 @@
 import { Center, Heading, Text, VStack } from "@chakra-ui/react";
+import { go_to_top } from "../../utils/util";
 
 export default function Logo() {
     return (
-        <Center>
-            <VStack w="250px" h="100%" spacing={0}>
+        <Center
+            _hover={{
+                cursor: "pointer",
+                color: "rgba(0, 0, 0, 0.7)",
+                transition: "all 0.2s linear",
+            }}
+            onClick={() => {
+                go_to_top(0);
+            }}
+        >
+            <VStack h="100%" spacing={0}>
                 <Heading h="70%" fontSize="25px">
                     KIMBYEONGSEOP
                 </Heading>
