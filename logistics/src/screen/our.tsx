ee2912eb29e272dd69e6118365e1c87805cc9263
Yuhyeon0516 <rladbgus0516@naver.com>
2023-11-01 13:43:27 +0900

    Applied go_to_scroll_position when first access page

--- logistics/src/screen/our.tsx
+++ logistics/src/screen/our.tsx
@@ -1,9 +1,15 @@
+import { useEffect } from "react";
 import PartenrsBringing from "../components/our/partenrsBringing";
 import PartnersReview from "../components/our/partnersReview";
 import Service from "../components/our/service";
 import Vision from "../components/our/vision";
+import { go_to_scroll_position } from "../utils/utils";
 
 export default function Our() {
+    useEffect(() => {
+        go_to_scroll_position(0);
+    }, []);
+
     return (
         <div>
             <Vision />
