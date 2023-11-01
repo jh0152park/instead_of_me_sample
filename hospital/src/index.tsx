a57de1ce113503e4efc93f1252bb589812998f11
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-11-02 00:24:39 +0900

    added global style

--- hospital/src/index.tsx
+++ hospital/src/index.tsx
@@ -1,8 +1,27 @@
 import ReactDOM from "react-dom/client";
-
 import App from "./App";
+import { reset } from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  body {
+    background-color: white;
+    color: black;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+  }
+`;
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+    <>
+        <GlobalStyle />
+        <App />
+    </>
+);
