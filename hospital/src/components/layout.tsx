e937f19b3c8d2b3137deecb5683e7be7f9fb58c7
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-11-02 21:11:23 +0900

    created logo at header

--- hospital/src/components/layout.tsx
+++ hospital/src/components/layout.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
-// import Header from "./header";
+import Header from "./header";
 
 export default function Layout() {
     return (
         <>
-            {/* <Header /> */}
+            <Header />
             <Outlet></Outlet>
         </>
     );
