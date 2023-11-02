c1a8b898c37988ebcfc53a87251f0c1455442dc9
Yuhyeon0516 <rladbgus0516@naver.com>
2023-11-02 14:39:53 +0900

    Created tax accountant project.

---
+++ tax_accountant/src/index.tsx
@@ -0,0 +1,31 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createGlobalStyle } from "styled-components";
+import { reset } from "styled-reset";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const GlobalStyle = createGlobalStyle`
+    ${reset};
+    body {
+        background-color: white;
+        color: black;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+
+  * {
+    box-sizing: border-box;
+    text-decoration: none;
+}
+`;
+
+const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+);
+root.render(
+    <>
+        <GlobalStyle />
+        <ChakraProvider>
+            <App />
+        </ChakraProvider>
+    </>
+);
