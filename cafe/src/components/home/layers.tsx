6d975a6c893312e778eb5ecedf85ee12763c59f5
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-12-08 19:20:24 +0900

    removed y scroll logic

--- cafe/src/components/home/layers.tsx
+++ cafe/src/components/home/layers.tsx
@@ -7,45 +7,37 @@ import Layer2 from "./layer2";
 import Layer3 from "./layer3";
 import Layer4 from "./layer4";
 
+let currentLayer = 1;
+let lastScrollY = 0;
+let lastDirection = "";
+let scrollStack = 0;
+const scrollTH = 10;
 export default function Layers() {
-    const { scrollY } = useScroll();
+    // window.addEventListener("scroll", (e) => {
+    //     const scrollY = window.scrollY;
+    //     const direction = scrollY > lastScrollY ? "down" : "up";
 
-    const scrollTH = 25;
-    const [scrollStack, setScrollStack] = useState<number>(0);
-    const [prevY, setPrevY] = useState<number>(0);
-    const [layer, setLayer] = useState<number>(1);
+    //     if (direction === lastDirection) {
+    //         scrollStack++;
+    //     } else {
+    //         scrollStack = 0;
+    //     }
 
-    useMotionValueEvent(scrollY, "change", (latest) => {
-        console.log(`y: ${latest}`);
-        if (latest < 0 || latest > 2800) return;
-
-        setScrollStack((prev) => prev + 1);
-        if (scrollStack > scrollTH) {
-            if (latest > prevY) {
-                // go to down
-                if (layer >= 4) {
-                    go_to_id("layer4");
-                    return;
-                }
-                go_to_id(`layer${layer + 1}`);
-                setLayer((prev) => prev + 1);
-            } else {
-                //go to up
-                if (layer <= 1) {
-                    go_to_id("layer1");
-                    return;
-                }
-                go_to_id(`layer${layer - 1}`);
-                setLayer((prev) => prev - 1);
-            }
-            setScrollStack(0);
-        }
-        setPrevY(latest);
-    });
-
-    useEffect(() => {
-        if (!scrollStack) sleep(300);
-    }, [scrollStack]);
+    //     if (scrollStack > scrollTH) {
+    //         scrollStack = 0;
+    //         if (direction === "down") {
+    //             currentLayer =
+    //                 currentLayer < 4 ? currentLayer + 1 : currentLayer;
+    //         } else {
+    //             currentLayer =
+    //                 currentLayer > 1 ? currentLayer - 1 : currentLayer;
+    //         }
+    //         go_to_id(`layer${currentLayer}`);
+    //         console.log(`detected, go to layer${currentLayer}`);
+    //     }
+    //     lastScrollY = scrollY;
+    //     lastDirection = direction;
+    // });
 
     return (
         <VStack w="100%">
