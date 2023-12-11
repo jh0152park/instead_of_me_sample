659ca6f33d88ae1f4929d7c8302c5989ffae92c2
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-12-11 22:31:11 +0900

    start to fixed main background images

--- cafe/src/components/home/layers.tsx
+++ cafe/src/components/home/layers.tsx
@@ -7,37 +7,13 @@ import Layer2 from "./layer2";
 import Layer3 from "./layer3";
 import Layer4 from "./layer4";
 
-let currentLayer = 1;
-let lastScrollY = 0;
-let lastDirection = "";
-let scrollStack = 0;
-const scrollTH = 10;
 export default function Layers() {
-    // window.addEventListener("scroll", (e) => {
-    //     const scrollY = window.scrollY;
-    //     const direction = scrollY > lastScrollY ? "down" : "up";
-
-    //     if (direction === lastDirection) {
-    //         scrollStack++;
-    //     } else {
-    //         scrollStack = 0;
-    //     }
-
-    //     if (scrollStack > scrollTH) {
-    //         scrollStack = 0;
-    //         if (direction === "down") {
-    //             currentLayer =
-    //                 currentLayer < 4 ? currentLayer + 1 : currentLayer;
-    //         } else {
-    //             currentLayer =
-    //                 currentLayer > 1 ? currentLayer - 1 : currentLayer;
-    //         }
-    //         go_to_id(`layer${currentLayer}`);
-    //         console.log(`detected, go to layer${currentLayer}`);
-    //     }
-    //     lastScrollY = scrollY;
-    //     lastDirection = direction;
-    // });
+    const images = [
+        "https://github.com/jh0152park/instead_of_me_sample/blob/main/cafe/images/image1.jpg?raw=true",
+        "https://github.com/jh0152park/instead_of_me_sample/blob/main/cafe/images/image2.jpg?raw=true",
+        "https://github.com/jh0152park/instead_of_me_sample/blob/main/cafe/images/image3.jpg?raw=true",
+        "https://github.com/jh0152park/instead_of_me_sample/blob/main/cafe/images/image4.jpg?raw=true",
+    ];
 
     return (
         <VStack w="100%">
