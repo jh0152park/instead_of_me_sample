1c8629c0b501281d95a3a696d0da4e872df9d0dd
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-12-12 23:59:09 +0900

    added best products at main page

--- cafe/src/components/home/products.tsx
+++ cafe/src/components/home/products.tsx
@@ -3,29 +3,52 @@ import {
     Box,
     Center,
     Divider,
+    Grid,
     HStack,
     Heading,
     Text,
+    VStack,
 } from "@chakra-ui/react";
+import Product from "../Products/product";
 
 const SCREEN_WIDTH = window.innerWidth;
 
 export default function Products() {
+    const products = [
+        require("../../resources/images/shop/product1.jpg"),
+        require("../../resources/images/shop/product2.jpg"),
+        require("../../resources/images/shop/product3.jpg"),
+        require("../../resources/images/shop/product4.jpg"),
+    ];
+
     return (
         <Center w="100%" h="100vh" bgColor="whiteAlpha.200">
-            <Box>
-                <HStack w="100%">
-                    <Box
-                        w={`${SCREEN_WIDTH * 0.15}px`}
-                        border="1px solid white"
-                    />
-                    <Heading>Products</Heading>
-                    <Box
-                        w={`${SCREEN_WIDTH * 0.15}px`}
-                        border="1px solid white"
-                    />
-                </HStack>
-            </Box>
+            <VStack>
+                <Box>
+                    <HStack w="100%">
+                        <Box
+                            w={`${SCREEN_WIDTH * 0.15}px`}
+                            border="1px solid white"
+                        />
+                        <Heading mx="20px">Best Products</Heading>
+                        <Box
+                            w={`${SCREEN_WIDTH * 0.15}px`}
+                            border="1px solid white"
+                        />
+                    </HStack>
+                </Box>
+
+                <Grid templateColumns="repeat(4, 1fr)" gap="30px">
+                    {products.map((product) => (
+                        <Product
+                            productImage={product}
+                            productCost={100}
+                            productDescription="test"
+                            productName="test"
+                        />
+                    ))}
+                </Grid>
+            </VStack>
         </Center>
     );
 }
