be2e3cdaf278c12cd453504b7dd0e4d4ecef125f
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-12-15 20:28:52 +0900

    added base path to header style

--- flower/src/components/header.tsx
+++ flower/src/components/header.tsx
@@ -5,6 +5,7 @@ import { go_to_top } from "../utils/util";
 export default function Header() {
     const navigate = useNavigate();
     const location = useLocation();
+    const basePath = ["/", "/instead_of_me_flower_shop"];
 
     return (
         <Center
@@ -13,24 +14,25 @@ export default function Header() {
             pt="50px"
             pb="20px"
             zIndex="99"
-            bgColor={location.pathname === "/" ? "rgba(0,0,0,0)" : "white"}
+            bgColor={
+                basePath.includes(location.pathname) ? "rgba(0,0,0,0)" : "white"
+            }
         >
             <Text
                 fontFamily={"Cormorant"}
                 fontSize={"50px"}
                 onClick={() => {
-                    if (location.pathname === "/") go_to_top(0);
+                    if (basePath.includes(location.pathname)) go_to_top(0);
                     else navigate("/");
                 }}
                 _hover={{
                     cursor: "pointer",
-                    color:
-                        location.pathname === "/"
-                            ? "rgba(255, 255, 255, 0.5)"
-                            : "rgba(0, 0, 0, 0.5)",
+                    color: basePath.includes(location.pathname)
+                        ? "rgba(255, 255, 255, 0.5)"
+                        : "rgba(0, 0, 0, 0.5)",
                     transition: "color 0.2s linear",
                 }}
-                color={location.pathname === "/" ? "white" : "black"}
+                color={basePath.includes(location.pathname) ? "white" : "black"}
             >
                 My Flower
             </Text>
